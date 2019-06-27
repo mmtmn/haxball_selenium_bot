2727f316e106ca3068cf548ff2e3c8fd0baa45ff
Thiago M. Nóbrega <42742390+mmtmn@users.noreply.github.com>
2019-06-27 14:03:45 -0300

    Add files via upload

--- create_a_Bot_on_Haxball.py
+++ create_a_Bot_on_Haxball.py
@@ -6,5 +6,9 @@ driver = webdriver.Chrome(executable_path="C:\\Users\\thiag\\Desktop\\Selenium\\
 driver.get("https://www.haxball.com/") #site do jogo
 driver.find_element_by_xpath("/html/body/div/div[2]/div[1]/a").click() #primeiro botão do jogo
 driver.switch_to.frame(driver.find_element_by_tag_name("iframe")) #selecione a frame!
-login_form = driver.find_element_by_xpath("/html/body/div/div/div/div/input").send_keys("mmtmn_bot") #cria o nome do personagem
+login_form = driver.find_element_by_xpath("/html/body/div/div/div/div/input").send_keys("mmtmn_bots") #cria o nome do personagem
 driver.find_element_by_xpath("/html/body/div/div/div/button").click() #seleciona botão para criar nick
+
+driver.get("https://www.haxball.com/play?c=WfEUn16I3Jk")
+driver.switch_to.frame(driver.find_element_by_tag_name("iframe")) #selecione a frame!
+driver.find_element_by_xpath("/html/body/div/div/div/button").click() #seleciona botão para entrar no jogo!
